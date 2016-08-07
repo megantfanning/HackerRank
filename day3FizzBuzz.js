794d7354ba3f8271bee63b46cf55afa75594a1d7
Megan Fanning <megantfanning@gmail.com>
2016-08-07 12:08:53 -0700

    fiz buzz js

---
+++ day3FizzBuzz.js
@@ -0,0 +1,37 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var N = parseInt(readLine());
+    if(N%2 == 0){
+        if(N<5){
+            process.stdout.write("Not Weird")      
+        }else if(N>5 && N<=20){
+            process.stdout.write("Weird")
+        }else if(N>20){
+            process.stdout.write("Not Weird")
+        }
+    }else{
+        process.stdout.write("Weird")
+    } 
+}
+
